26db0138d8907466deda633b3236301c8aa1757e
masaki.arizuka <masaki.arizuka@gmail.com>
2018-09-17 23:13:37 +0900

    save

---
+++ ABC108/D/main.rs
@@ -0,0 +1,85 @@
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    input!{
+        l: u32
+    }
+
+    let mut r = 0;
+    while 2u32.pow(r+1) <= l { r += 1 };
+
+    let mut ans = vec![];
+
+    for i in 0..r {
+        ans.push((i+1, i+2, 0));
+        ans.push((i+1, i+2, 2u32.pow(i)));
+    }
+
+    let mut t = l - 2u32.pow(r);
+    for i in (0..r).rev() {
+        let p = 2u32.pow(i);
+        if p <= t {
+            t -= p;
+            ans.push( (i+1, r+1, 2u32.pow(r) + 2u32.pow(i)) ) ;
+        }
+    }
+
+    println!("{} {}", r+1, ans.len());
+    for &(i, j, c) in ans.iter() {
+        println!("{} {} {}", i, j, c);
+    }
+}
