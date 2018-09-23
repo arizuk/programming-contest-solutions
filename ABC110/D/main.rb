fa97c753c8d04312a3fd9a356a821adadae3f110
masaki.arizuka <masaki.arizuka@gmail.com>
2018-09-24 03:24:05 +0900

    save

--- ABC110/D/main.rb
+++ ABC110/D/main.rb
@@ -1,21 +1,24 @@
 require 'prime'
 
-def combi(n, a, index)
-  if n == 0
-    p a
-    return
+def mod_combi(n, m)
+  return mod_combi(n, n-m) if n-m < m;
+
+  ans = 1
+  for i in 0...m do
+    ans *= n - i;
+    ans /= i + 1;
   end
-  for i in 1..n do
-    a[index..index+n] = 1
-    combi(n - i, a, index+i)
-    a[index..index+n] = 0
-  end
+  # puts "#{n} #{m} -> #{ans}"
+  ans
 end
 
-factors = 60.prime_division
-a = []
+mod = 10**9 + 7;
+n, m = STDIN.gets.split(" ").map(&:to_i)
+factors = m.prime_division
+
+ans = 1
 factors.each do |v, pow|
-  pow.times { a << v }
+  ans *= mod_combi(n+pow-1, n-1)
 end
-# combi(a, 0, a.length-1)
-combi(5, Array.new(5, 0), 0)
+puts ans % mod
+
