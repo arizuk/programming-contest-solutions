e65a3091ee37e752170fdbc1273cead8c98ee762
masaki.arizuka <masaki.arizuka@gmail.com>
2018-10-08 01:59:58 +0900

    save

---
+++ ABC095/C/main.rs
@@ -0,0 +1,85 @@
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_imports)]
+use std::cmp::{min, max};
+
+fn main() {
+    input!{
+        a: i64,
+        b: i64,
+        c: i64,
+        x: i64,
+        y: i64,
+    }
+
+    let mut ans = 1 << 32;
+    for aa in 0..x+1 {
+        let cc = 2*x - 2*aa;
+        let bb = max(0, y - cc/2);
+        let p = aa*a + bb*b + cc*c;
+        ans = min(ans, p);
+    }
+
+    for bb in 0..y+1 {
+        let cc = 2*y - 2*bb;
+        let aa = max(0, x - cc/2);
+        let p = aa*a + bb*b + cc*c;
+        ans = min(ans, p);
+    }
+
+    println!("{}", ans);
+}
