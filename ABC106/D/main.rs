8c4f0f649626884a43c2a1743c4c5364514c47dd
masaki.arizuka <masaki.arizuka@gmail.com>
2018-10-16 23:12:38 +0900

    save

---
+++ ABC106/D/main.rs
@@ -0,0 +1,96 @@
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($a:expr),*) => {
+        println!(concat!($(stringify!($a), "={:?} "),*), $($a),*);
+    }
+}
+
+#[allow(unused_imports)]
+use std::cmp::{min, max};
+
+fn main() {
+    input!{
+      n: usize,
+      m: usize,
+      q: usize,
+      lrs: [[usize; 2]; m],
+      pqs: [[usize; 2]; q],
+    }
+    let mut ss = vec![vec![0; n+1]; n+1];
+    for i in 0..m {
+        let l = lrs[i][0];
+        let r = lrs[i][1];
+        ss[l][r] += 1;
+    }
+    for l in 1..n+1 {
+        for r in 1..n+1 {
+            ss[l][r] = ss[l][r-1] + ss[l][r];
+        }
+    }
+    for i in 0..q {
+        let p = pqs[i][0];
+        let q = pqs[i][1];
+
+        let mut ans = 0;
+        for l in p..q+1 {
+            ans += ss[l][q] - ss[l][p-1];
+        }
+        println!("{}", ans);
+    }
+}
