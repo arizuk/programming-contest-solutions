d68a0ecb17b6a2c09eb4e3ed341a7c81a1636493
masaki.arizuka <masaki.arizuka@gmail.com>
2018-11-06 21:44:15 +0900

    save

---
+++ ABC072/C/main.rs
@@ -0,0 +1,90 @@
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($a:expr),*) => {
+        println!(concat!($(stringify!($a), "={:?} "),*), $($a),*);
+    }
+}
+
+#[allow(unused_imports)]
+use std::cmp::{min, max};
+use std::collections::HashMap;
+
+fn main() {
+    input!{
+      n: usize,
+      aa: [i64; n],
+    }
+    let mut map: HashMap<i64, i64> = HashMap::new();
+    for &a in &aa {
+        let e = map.entry(a).or_insert(0);
+        *e += 1;
+
+        let e = map.entry(a+1).or_insert(0);
+        *e += 1;
+        // for &v in [a, a+1, a-1].iter() {
+        //     let e = map.entry(v).or_insert(0);
+        //     *e += 1;
+        // }
+    }
+    let mut ans = 0;
+    for (_k, &v) in map.iter() {
+        ans = max(ans, v);
+    }
+    println!("{}", ans);
+}
