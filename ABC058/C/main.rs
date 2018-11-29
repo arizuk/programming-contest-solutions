01ced0107074755a79a761e7238d3a3b619e078c
masaki.arizuka <masaki.arizuka@gmail.com>
2018-11-30 00:47:39 +0900

    save

---
+++ ABC058/C/main.rs
@@ -0,0 +1,91 @@
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($a:expr),*) => {
+        println!(concat!($(stringify!($a), "={:?} "),*), $($a),*);
+    }
+}
+
+#[allow(unused_imports)]
+use std::cmp::{min, max};
+
+fn main() {
+    input!{
+      n: usize,
+      ss: [chars; n],
+    }
+
+    let mut letters = vec![vec![0; n]; 26];
+    for i in 0..n {
+        for &s in &ss[i] {
+            letters[s as usize - 'a' as usize][i] += 1;
+        }
+    }
+    let mut ans = String::new();
+    for i in 0..26 {
+        let c = ('a' as u8 + i) as char;
+        let mut min_value = letters[i as usize][0];
+        for &v in letters[i as usize].iter() {
+            min_value = min(v, min_value);
+        }
+        for _ in 0..min_value {
+            ans = ans + &c.to_string();
+        }
+    }
+    println!("{}", ans);
+}
