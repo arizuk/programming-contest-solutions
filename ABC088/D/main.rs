95d81b539dad291b20e8e1462aa8d7c5c7178f4a
masaki.arizuka <masaki.arizuka@gmail.com>
2018-11-30 00:15:26 +0900

    Use breadth first search rather than depth first search.

--- ABC088/D/main.rs
+++ ABC088/D/main.rs
@@ -7,53 +7,47 @@ macro_rules! debug {
 
 #[allow(unused_imports)]
 use std::cmp::{min, max};
-
-fn search(ss: &Vec<Vec<char>>, i: i64, j: i64, scores: &mut Vec<Vec<i64>>, score: i64) {
-    let h = ss.len() as i64;
-    let w = ss[0].len() as i64;
-    let n_score = score + 1;
-
-    for &(dx, dy) in [(1, 0), (-1, 0), (0, 1), (0, -1)].iter() {
-        let nx = i + dx;
-        let ny = j + dy;
-        if nx < 0 || nx >= h { continue };
-        if ny < 0 || ny >= w { continue };
-        let nx = nx as usize;
-        let ny = ny as usize;
-        if ss[nx][ny] == '#' { continue };
-
-        if n_score >= scores[nx][ny] && scores[nx][ny] != -1 {
-            continue;
-        }
-        scores[nx][ny] = n_score;
-        search(ss, nx as i64, ny as i64, scores, n_score);
-    }
-}
-
+use std::collections::VecDeque;
 
 fn main() {
     let stdin = std::io::stdin();
-    let mut buf = String::new();
-    stdin.read_line(&mut buf).unwrap();
-    let hws = buf.split_whitespace().map(|v| v.parse().unwrap()).collect::<Vec<usize>>();
+    let mut sc = Scanner { reader: stdin };
 
-    let h = hws[0];
-    let w = hws[1];
+    let h: usize = sc.read();
+    let w: usize = sc.read();
 
     let mut ss = vec![];
     for _ in 0..h {
-        let mut buf = String::new();
-        stdin.read_line(&mut buf).unwrap();
-        let buf = buf.trim_right();
-        let row = buf.chars().collect::<Vec<char>>();
-        ss.push(row);
+        ss.push(sc.chars());
     }
 
-    let mut scores = vec![vec![-1i64; w]; h];
-    scores[0][0] = 0;
-    search(&ss, 0, 0, &mut scores, 0);
+    let mut costs = vec![vec![-1i64; w]; h];
+    costs[0][0] = 0;
 
-    let score = scores[h-1][w-1];
+    let mut q: VecDeque<(i64, i64, i64)> = VecDeque::new();
+    q.push_back((0, 0, 1));
+    while q.len() >= 1 {
+        let (x, y, c) = q.pop_front().unwrap();
+        let n_cost = c + 1;
+        for &(dx, dy) in [(1, 0), (-1, 0), (0, 1), (0, -1)].iter() {
+            let nx = x + dx;
+            let ny = y + dy;
+            if nx < 0 || nx >= h as i64 { continue };
+            if ny < 0 || ny >= w as i64 { continue };
+
+            let nx = nx as usize;
+            let ny = ny as usize;
+            if ss[nx][ny] == '#' { continue };
+
+            if n_cost >= costs[nx][ny] && costs[nx][ny] != -1 {
+                continue;
+            }
+            costs[nx][ny] = n_cost;
+            q.push_back((nx as i64, ny as i64, n_cost));
+        }
+    }
+
+    let cost = costs[h-1][w-1];
     let mut cnt = 0;
     for row in ss {
         for c in row {
@@ -62,9 +56,37 @@ fn main() {
             }
         }
     }
-    if score == -1 {
+    if cost == -1 {
         println!("{}", -1);
     } else {
-        println!("{}", cnt-score-1);
+        println!("{}", cnt-cost);
+    }
+}
+
+
+pub struct Scanner<R> {
+    reader: R,
+}
+impl<R: std::io::Read> Scanner<R> {
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .reader
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n')
+            .take_while(|&b| b != b' ' && b != b'\n')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn read_vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+    pub fn chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
     }
 }
