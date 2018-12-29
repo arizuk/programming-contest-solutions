795f8ab7240519da83de8ab5e0cbe1db52aa14f4
masaki.arizuka <masaki.arizuka@gmail.com>
2018-12-29 20:14:59 +0900

    save

---
+++ atcoder/AGC016/A/main.rs
@@ -0,0 +1,100 @@
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($a:expr),*) => {
+        #[cfg(debug_assertions)]
+        writeln!(&mut std::io::stderr(), concat!("[DEBUG] ", $(stringify!($a), "={:?} "),*), $($a),*);
+    }
+}
+
+#[allow(unused_imports)]
+use std::cmp::{min, max};
+
+#[allow(unused_imports)]
+use std::io::Write;
+
+fn solve(mut s: Vec<char>, c: char) -> usize {
+    let mut cnt = 0;
+    loop {
+        let ok = s.iter().fold(true, |acc, ci| acc && *ci == c);
+        if ok {
+            // debug!(c, cnt, s);
+            return cnt;
+        }
+        cnt += 1;
+
+        for i in 0..s.len()-1 {
+            if s[i+1] == c {
+                s[i] = c;
+            }
+        }
+        s.pop();
+    }
+}
+
+fn main() {
+    input!{
+      s: String
+    }
+    let mut ans = 101;
+    let s: Vec<char> = s.chars().collect();
+    for &c in s.iter() {
+        ans = min(ans, solve(s.clone(), c));
+    }
+    println!("{}", ans);
+}
