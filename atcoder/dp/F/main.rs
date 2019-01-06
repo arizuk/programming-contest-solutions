e2a06bd1c673bbd593e732490922d721a22add89
masaki.arizuka <masaki.arizuka@gmail.com>
2019-01-06 23:03:44 +0900

    Longest common subsequence

---
+++ atcoder/dp/F/main.rs
@@ -0,0 +1,107 @@
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($a:expr),*) => {
+        #[cfg(debug_assertions)]
+        writeln!(&mut std::io::stderr(), concat!("[DEBUG] ", $(stringify!($a), "={:?} "),*), $($a),*);
+    }
+}
+
+#[allow(unused_imports)]
+use std::cmp::{min, max};
+
+#[allow(unused_imports)]
+use std::io::Write;
+
+fn print_lcs(a: &Vec<char>, b: &Vec<char>, lcs: &Vec<Vec<usize>>, i: usize, j: usize) {
+    if i == 0 || j == 0 {
+        return;
+    }
+    if a[i-1] == b[j-1] {
+        print_lcs(a, b, lcs, i-1, j-1);
+        print!("{}", a[i-1]);
+    } else {
+        if lcs[i-1][j] >= lcs[i][j-1] {
+            print_lcs(a, b, lcs, i-1, j);
+        } else {
+            print_lcs(a, b, lcs, i, j-1);
+        }
+    }
+}
+
+fn main() {
+    input!{
+      s: chars,
+      t: chars,
+    }
+
+    let mut dp = vec![vec![0; 3000]; 3000];
+    let n = s.len();
+    let m = t.len();
+
+    for i in 0..n {
+        for j in 0..m {
+            if s[i] == t[j] {
+                dp[i+1][j+1] = dp[i][j] + 1;
+            } else {
+                dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j]);
+            }
+        }
+    }
+    print_lcs(&s, &t, &dp, n, m);
+}
