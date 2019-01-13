c72fb19554e88bde2591833f0eeb9b7b7e56c255
masaki.arizuka <masaki.arizuka@gmail.com>
2019-01-13 20:57:32 +0900

    save

--- atcoder/aising2019/D/main.rs
+++ atcoder/aising2019/D/main.rs
@@ -68,33 +68,75 @@ use std::cmp::{min, max};
 #[allow(unused_imports)]
 use std::io::Write;
 
+// [l, r)
+fn bs(mut l: usize, mut r: usize, f: &Fn(usize) -> bool) -> usize {
+    while r-l > 1 {
+        let mid = (r+l)/2;
+        if f(mid) {
+            l = mid;
+        } else {
+            r = mid;
+        }
+    }
+    l
+}
+
 fn main() {
     input!{
       n: usize,
       q: usize,
-      aa: [usize; n],
-      xs: [usize; q],
+      aa: [i64; n],
+      xs: [i64; q],
     }
 
-    let mut s0 = vec![0; n+1];
-    for i in 0..n {
-        s0[i+1] = s0[i] + aa[i];
-    }
+    let mut s = vec![0; n+2];
 
-    let mut s1 = vec![0; n+2];
-    for i in 0..n+2 {
-        if i+2 <= n+1 {
-            s1[i+2] = s1[i] + aa[i];
+    {
+        let mut s0 = vec![0; n+1];
+        for i in 0..n {
+            s0[i+1] = s0[i] + aa[i];
+        }
+
+        let mut s1 = vec![0; n+2];
+        for i in 0..n+2 {
+            if i+2 <= n+1 {
+                s1[i+2] = s1[i] + aa[i];
+            }
+        }
+
+        for i in 1..(n/2+2) {
+            let mut sum = s0[n] - s0[n-i];
+            if 2*i < n {
+                sum += s1[n+1-2*i];
+            }
+            s[i] = sum;
         }
     }
 
-    let mut s = vec![0; n];
-    for i in 1..(n/2+2) {
-        let mut sum = s0[n] - s0[n-i];
-        if 2*i < n {
-            sum += s1[n+1-2*i];
-        }
-        s[i] = sum;
+    for &x in xs.iter() {
+        let f = |mut m: usize| -> bool {
+            let mut k = n-m;
+            if m == 0 {
+                return true;
+            };
+            if m >= n-1 {
+                return false;
+            }
+
+            if k%2 == 1 {
+                m += 1;
+                k = n-m;
+            }
+
+            let li = m-1;
+            let ri = m+k/2-1;
+            let l = (aa[li] - x).abs();
+            let r = (aa[ri] - x).abs();
+            // debug!(n, m, k, li, ri, l, r);
+            if l > r { true } else { false }
+        };
+
+        let l = bs(0, n, &f);
+        println!("{}", s[(n-l)/2 + (n-l)%2]);
     }
-    // debug!(s);
 }
