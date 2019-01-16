07061ca312648f74a7c7b5eb52338d7aa10034cf
masaki.arizuka <masaki.arizuka@gmail.com>
2019-01-16 21:16:27 +0900

    save

---
+++ atcoder/ABC071/D/main.rs
@@ -0,0 +1,106 @@
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($a:expr),*) => {
+        #[cfg(debug_assertions)]
+        writeln!(&mut std::io::stderr(), concat!("[DEBUG] ", $(stringify!($a), "={:?} "),*), $($a),*);
+    }
+}
+
+#[allow(unused_imports)]
+use std::cmp::{min, max};
+
+#[allow(unused_imports)]
+use std::io::Write;
+
+fn main() {
+    input!{
+      n: usize,
+      s1: chars,
+      s2: chars,
+    }
+
+    const MOD: usize = 1e9 as usize + 7;
+    let mut prev = 0;
+    let mut i = 0;
+    let mut ans = 1;
+    while i < n {
+        if s1[i] == s2[i] {
+            i += 1;
+            ans *= match prev {
+                0 => 3,
+                1 => 2,
+                2 => 1,
+                _ => panic!()
+            };
+            ans %= MOD;
+            prev = 1;
+        } else {
+            i += 2;
+            ans *= match prev {
+                0 => 6,
+                1 => 2,
+                2 => 3,
+                _ => panic!()
+            };
+            ans %= MOD;
+            prev = 2;
+        }
+    }
+    println!("{}", ans);
+}
