a4fcaacfac3d3bfba696e7bd0d23ee2c95d9e24d
masaki.arizuka <masaki.arizuka@gmail.com>
2019-01-29 02:19:11 +0900

    save

---
+++ atcoder/ARC075/D/main.rs
@@ -0,0 +1,101 @@
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($a:expr),*) => {
+        #[cfg(debug_assertions)]
+        writeln!(&mut std::io::stderr(), concat!("[DEBUG] ", $(stringify!($a), "={:?} "),*), $($a),*);
+    }
+}
+
+#[allow(unused_imports)]
+use std::cmp::{min, max};
+
+#[allow(unused_imports)]
+use std::io::Write;
+
+fn ok(hs: &[i64], n: i64, a: i64, b: i64) -> bool {
+    let d = a - b;
+    let mut cnt = 0;
+    for i in 0..hs.len() {
+        let r = hs[i] - b*n;
+        cnt += (r + d - 1) / d;
+    }
+    cnt <= n
+}
+
+fn main() {
+    input!{
+      n: usize,
+      a: i64,
+      b: i64,
+      hs: [i64; n],
+    }
+
+    let mut l = 0i64;
+    let mut r = *hs.iter().max().unwrap() / b + 1;
+
+    while r-l > 1 {
+        let mid = (r+l)/2;
+        if ok(&hs, mid, a, b) {
+            r = mid;
+        } else {
+            l = mid;
+        }
+    }
+    println!("{}", r);
+}
