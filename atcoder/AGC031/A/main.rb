e0c412299d3673527eb25d4f18d78a7954223726
masaki.arizuka <masaki.arizuka@gmail.com>
2019-03-19 22:25:33 +0900

    AGC031

---
+++ atcoder/AGC031/A/main.rb
@@ -0,0 +1,7 @@
+n = STDIN.gets.to_i
+ss = STDIN.gets.strip.split
+p ss
+ss.each do |c|
+  puts c
+end
+
---
+++ atcoder/AGC031/B/main.rs
@@ -0,0 +1,102 @@
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+
+    ($next:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($a:expr),*) => {
+        #[cfg(debug_assertions)]
+        writeln!(&mut std::io::stderr(), concat!("[DEBUG] ", $(stringify!($a), "={:?} "),*), $($a),*);
+    }
+}
+
+#[allow(unused_imports)]
+use std::cmp::{min, max};
+
+#[allow(unused_imports)]
+use std::io::Write;
+type I = usize;
+const MOD: usize = 1e9 as usize + 7;
+
+fn main() {
+    input!{
+      n: usize,
+      mut cs1: [usize; n],
+    }
+    let mut cs = vec![];
+    for i in 1..n {
+        if cs1[i] != cs1[i-1] {
+            cs.push(cs1[i-1]);
+        }
+    }
+    cs.push(cs1[n-1]);
+    let n = cs.len();
+    let mut ans = 1;
+    let mut num = vec![0; 200001];
+    for i in 0..n {
+        if num[cs[i]] > 0 {
+            ans += num[cs[i]];
+            ans %= MOD;
+        }
+        num[cs[i]] = ans;
+    }
+    // debug!(num);
+    println!("{}", ans);
+}
