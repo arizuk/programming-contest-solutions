e2df46e91a079ef8581c4e73bc008da348e2a4fa
masaki.arizuka <masaki.arizuka@gmail.com>
2019-05-09 02:48:41 +0900

    save

---
+++ atcoder/cpsco2019-s1/C/main.rs
@@ -0,0 +1,110 @@
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+
+    ($next:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($a:expr),*) => {
+        #[cfg(debug_assertions)]
+        writeln!(&mut std::io::stderr(), concat!("[DEBUG] ", $(stringify!($a), "={:?} "),*), $($a),*);
+    }
+}
+
+#[allow(unused_imports)]
+use std::cmp::{min, max};
+
+#[allow(unused_imports)]
+use std::io::Write;
+
+fn dfs(aa: &[usize], rem: usize, sum: usize, i: usize) -> usize {
+    if rem == 0 {
+        return number_of_coins(sum)
+    }
+    let mut ans = 1 << 31;
+    for ni in i..aa.len() {
+        ans = min(
+            ans,
+            dfs(aa, rem-1, sum+aa[ni], ni+1)
+        );
+    }
+    ans
+}
+
+fn number_of_coins(mut price: usize) -> usize {
+    let mut ans = 0;
+    while price > 0 {
+        let d = price%10;
+        ans += d%5;
+        if d >= 5 {
+            ans += 1;
+        }
+        price /= 10;
+    }
+    ans
+}
+
+fn main() {
+    input!{
+      n: usize,
+      k: usize,
+      aa: [usize; n],
+    }
+    println!("{}", dfs(&aa, k, 0, 0));
+}
