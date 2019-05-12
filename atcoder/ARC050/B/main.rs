f6ded71dd979efdbd3c1cc8779d0cd2114e209d7
masaki.arizuka <masaki.arizuka@gmail.com>
2019-05-12 22:59:29 +0900

    Save

---
+++ atcoder/ARC050/B/main.rs
@@ -0,0 +1,112 @@
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+
+    ($next:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($a:expr),*) => {
+        #[cfg(debug_assertions)]
+        writeln!(&mut std::io::stderr(), concat!("[DEBUG] ", $(stringify!($a), "={:?} "),*), $($a),*);
+    }
+}
+
+#[allow(unused_imports)]
+use std::cmp::{min, max};
+
+#[allow(unused_imports)]
+use std::io::Write;
+type I = usize;
+
+// [l, r]
+fn binary_search<F>(mut l: i64, mut r: i64, f: &F) -> Option<i64>
+  where F: Fn(i64) -> bool
+  {
+    assert!(l <= r);
+    r += 1;
+    let r_bound = r;
+    while r > l {
+        let m = l + (r - l) / 2;  // avoid overflow
+        if f(m) {
+          r = m;
+        } else {
+          if l == m { break; }
+          l = m;
+        }
+    }
+    if r == r_bound { None } else { Some(r) }
+}
+
+fn main() {
+    input!{
+      r: i64,
+      b: i64,
+      x: i64,
+      y: i64,
+    }
+
+    let ng = |k| {
+        let r = r - k;
+        let b = b - k;
+        let ok = r >= 0 && b >= 0 && (r/(x-1) + b/(y-1)) >= k;
+        !ok
+    };
+    let ans = binary_search(0, r+b, &ng).unwrap();
+    println!("{}", ans-1);
+}
