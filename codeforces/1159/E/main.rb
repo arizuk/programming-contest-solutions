e11a07d658e4c07dac8044c4b4849cc4c21f8f03
masaki.arizuka <masaki.arizuka@gmail.com>
2019-05-19 20:59:49 +0900

    save

--- codeforces/1159/E/main.rb
+++ codeforces/1159/E/main.rb
@@ -14,19 +14,31 @@ class Node
   end
 end
 
+def ok?(ps, nxs)
+  n = nxs.length
+  q = []
+  (n-1).downto(0) do |i|
+    q.pop while !q.empty? && ps[q[-1]] < ps[i]
+    return false if q.empty? && nxs[i] != n+1
+    return false if !q.empty? && nxs[i] != q[-1]+1
+    q.push(i)
+  end
+end
+
 T = STDIN.gets.to_i
 T.times do
   n = STDIN.gets.to_i
   nxs = STDIN.gets.strip.split.map(&:to_i)
+  for i  in 0..n
+    nxs[i] = i+2 if nxs[i] == -1
+  end
 
   nodes = {}
   nxs.each.with_index(1) do |nx, i|
     nodes[i] ||= Node.new(i)
-    if nx == -1
-    else
-      nodes[nx] ||= Node.new(nx)
-      nodes[nx].add(nodes[i])
-    end
+    # nx = i+1 if nx == -1
+    nodes[nx] ||= Node.new(nx)
+    nodes[nx].add(nodes[i])
   end
 
   ans = []
@@ -39,5 +51,9 @@ T.times do
   end
   ans = ans.sort_by { |v| v[0] }.map { |v| v[1] }
   ans.pop
-  puts ans.join(" ")
+  if ok?(ans, nxs)
+    puts ans.join(" ")
+  else
+    puts -1
+  end
 end
