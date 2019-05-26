6aae14de7c38808d95132a20641fde78a78c5203
masaki.arizuka <masaki.arizuka@gmail.com>
2019-05-26 13:41:51 +0900

    ABC127-E

--- atcoder/ABC127/E/main.rs
+++ atcoder/ABC127/E/main.rs
@@ -72,6 +72,41 @@ use std::cmp::{min, max};
 
 #[allow(unused_imports)]
 use std::io::Write;
+const MAX: usize = 1e5 as usize * 2 + 1;
+const MOD: usize = 1e9 as usize + 7;
+
+#[allow(dead_code)]
+pub struct ModDivision {
+    fact: Vec<usize>,
+    inv: Vec<usize>,
+    finv: Vec<usize>,
+}
+impl ModDivision {
+    pub fn new(max_value: usize) -> Self {
+        let mut fact = vec![0; max_value + 1];
+        let mut inv = vec![0; max_value + 1];
+        let mut finv = vec![0; max_value + 1];
+        fact[0] = 1;
+        fact[1] = 1;
+        finv[0] = 1;
+        finv[1] = 1;
+        inv[1] = 1;
+        for i in 2..max_value + 1 {
+            fact[i] = fact[i - 1] * i % MOD;
+            inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
+            finv[i] = finv[i - 1] * inv[i] % MOD;
+        }
+        ModDivision {
+            fact: fact,
+            inv: inv,
+            finv: finv,
+        }
+    }
+    pub fn combination(&self, n: usize, k: usize) -> usize {
+        assert!(n >= k);
+        self.fact[n] * self.finv[n - k] % MOD * self.finv[k] % MOD
+    }
+}
 
 fn main() {
     input!{
@@ -79,4 +114,16 @@ fn main() {
       m: usize,
       k: usize,
     }
+    let mut ans = 0;
+    let div = ModDivision::new(MAX);
+    let pair = div.combination(n*m-2,k-2);
+    for d in 1..m {
+        ans += n*n*(m-d)*d%MOD *pair%MOD;
+        ans %= MOD;
+    }
+    for d in 1..n {
+        ans += m*m*(n-d)*d%MOD *pair%MOD;
+        ans %= MOD;
+    }
+    println!("{}", ans);
 }
