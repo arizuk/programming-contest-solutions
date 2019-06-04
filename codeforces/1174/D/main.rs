a50d0c7fa8e7dc075f1298ba6da0b6e242565741
masaki.arizuka <masaki.arizuka@gmail.com>
2019-06-05 01:23:21 +0900

    save

---
+++ codeforces/1174/D/main.rs
@@ -0,0 +1,110 @@
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+
+    ($next:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($a:expr),*) => {
+        #[cfg(debug_assertions)]
+        writeln!(&mut std::io::stderr(), concat!("[DEBUG] ", $(stringify!($a), "={:?} "),*), $($a),*);
+    }
+}
+
+#[allow(unused_imports)]
+use std::cmp::{min, max};
+
+#[allow(unused_imports)]
+use std::io::Write;
+
+fn main() {
+    input!{
+      n: usize,
+      x: usize,
+    }
+    let max_a = 1 << n;
+    let mut pairs = vec![];
+    for a in 1..max_a {
+        if a != x {
+            let b = a^x;
+            if a < b {
+                pairs.push((a,b));
+            } else {
+                pairs.push((b,a));
+            }
+        }
+    }
+    pairs.sort();
+    pairs.dedup();
+    let mut bs: Vec<usize> = pairs.into_iter().map(|v| v.0).collect();
+
+    println!("{}", bs.len());
+
+    if bs.len() > 0 {
+        let mut aa = vec![];
+        aa.push(bs[0]);
+        for i in 1..bs.len() {
+            aa.push(bs[i] ^ bs[i-1]);
+        }
+        for a in aa {
+            print!("{} ", a);
+        }
+        println!();
+    }
+}
