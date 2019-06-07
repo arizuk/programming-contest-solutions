ca05591a0ff08a06009230795cc1e2d6a051e06b
masaki.arizuka <masaki.arizuka@gmail.com>
2019-06-08 00:54:20 +0900

    save

---
+++ atcoder/AGC008/A/main.rs
@@ -0,0 +1,100 @@
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+
+    ($next:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($a:expr),*) => {
+        #[cfg(debug_assertions)]
+        writeln!(&mut std::io::stderr(), concat!("[DEBUG] ", $(stringify!($a), "={:?} "),*), $($a),*);
+    }
+}
+
+#[allow(unused_imports)]
+use std::cmp::{min, max};
+
+#[allow(unused_imports)]
+use std::io::Write;
+
+fn main() {
+    input!{
+      mut x: i64,
+      mut y: i64,
+    }
+    let mut ans = 1 << 32;
+    // Bを0 or 1 回押す
+    // Aを0回以上押す
+    // Bを0 or 1 回押す
+
+    // Aを0回以上押せるのは基本的に、x<=yのとき。
+    if x <= y {
+        ans = min(ans, y-x);
+    }
+    if -x <= y { // 最初に反転する場合。-x が y より小さければ条件が成立する
+        ans = min(ans, y+x+1);
+    }
+    if x <= -y { // 最後に反転する場合。足しているときは符号が違う。x <= -y
+        ans = min(ans, -y-x+1);
+    }
+    if -x <= -y { // 2回反転。-x <= -y
+        ans = min(ans, -y+x+2);
+    }
+    println!("{}", ans);
+}
