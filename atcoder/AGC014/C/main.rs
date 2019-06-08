fc1cbda40179e63f79ef0d5e63c36155fabb1898
masaki.arizuka <masaki.arizuka@gmail.com>
2019-06-08 21:19:19 +0900

    Closed room

--- atcoder/AGC014/C/main.rs
+++ atcoder/AGC014/C/main.rs
@@ -118,49 +118,7 @@ fn main() {
       s: [chars; h],
     }
 
-    const INF: usize = 1 << 32;
-    let mut cost = vec![vec![INF; w]; h];
-    use std::collections::BinaryHeap;
-    let mut q = BinaryHeap::new();
-    for &i in [0, h-1].iter() {
-        for j in 0..w {
-            if s[i][j] == '#' {
-                q.push((Rev(1), i, j));
-                cost[i][j] = 1;
-            } else {
-                q.push((Rev(0), i, j));
-                cost[i][j] = 0;
-            }
-        }
-    }
-    for &j in [0, w-1].iter() {
-        for i in 0..h {
-            if s[i][j] == '#' {
-                q.push((Rev(1), i, j));
-                cost[i][j] = 1;
-            } else {
-                q.push((Rev(0), i, j));
-                cost[i][j] = 0;
-            }
-        }
-    }
-
-    while let Some((Rev(c), i, j)) = q.pop() {
-        if cost[i][j] < c {
-            continue;
-        }
-        for (nx, ny) in next_points(i, j, h, w) {
-            let mut next_cost = c;
-            if s[nx][ny] == '#' {
-                next_cost += 1;
-            }
-            if cost[nx][ny] > next_cost {
-                cost[nx][ny] = next_cost;
-                q.push((Rev(next_cost), nx, ny));
-            }
-        }
-    }
-
+    let mut visited = vec![vec![false; w]; h];
     let mut start = (0,0);
     for i in 0..h {
         for j in 0..w {
@@ -170,23 +128,19 @@ fn main() {
             }
         }
     }
-
-    let mut visited = vec![vec![false; w]; h];
-
     let mut q = std::collections::VecDeque::new();
     q.push_back((0, start.0, start.1));
     visited[start.0][start.1] = true;
-
-    debug!(cost);
-
-    let mut ans = INF;
+    let mut ans = h*w + 1;
     while let Some((c, x, y)) = q.pop_front() {
-        ans = min(ans, cost[x][y]);
+        let d = min(min(x, y), min(h-x-1, w-y-1));
+        let num = (d + k - 1) /k + 1;
+        ans = min(ans, num);
         if c >= k {
             continue;
         }
         for (nx, ny) in next_points(x, y, h, w) {
-            if s[ny][ny] != '#' && !visited[nx][ny] {
+            if s[nx][ny] != '#' && !visited[nx][ny] {
                 visited[nx][ny] = true;
                 q.push_back((c+1, nx, ny));
             }
