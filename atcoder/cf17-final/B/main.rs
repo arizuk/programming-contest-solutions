c8aae2426a39df7ff2f9b05d0660027ea67607bd
masaki.arizuka <masaki.arizuka@gmail.com>
2019-06-14 02:43:40 +0900

    save

---
+++ atcoder/cf17-final/B/main.rs
@@ -0,0 +1,95 @@
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+
+    ($next:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($a:expr),*) => {
+        #[cfg(debug_assertions)]
+        writeln!(&mut std::io::stderr(), concat!("[DEBUG] ", $(stringify!($a), "={:?} "),*), $($a),*);
+    }
+}
+
+#[allow(unused_imports)]
+use std::cmp::{min, max};
+
+#[allow(unused_imports)]
+use std::io::Write;
+
+fn main() {
+    input!{
+      s: chars,
+    }
+    let mut cnts = vec![0; 3];
+    for c in s {
+        let idx = (c as u8 - 'a' as u8) as usize;
+        cnts[idx] += 1;
+    }
+    let a = cnts[0];
+    let b = cnts[1];
+    let c = cnts[2];
+    let max_n = max(a, max(b, c));
+    let min_n = min(a, min(b, c));
+    if max_n-min_n >= 2 {
+        println!("{}", "NO");
+    } else {
+        println!("{}", "YES");
+    }
+}
