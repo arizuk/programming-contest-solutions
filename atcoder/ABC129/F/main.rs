c05af18b7d39ea485d0f4faf238da01c2638c301
masaki.arizuka <masaki.arizuka@gmail.com>
2019-06-15 19:01:16 +0900

    Matric exponentation

--- atcoder/ABC129/F/main.rs
+++ atcoder/ABC129/F/main.rs
@@ -73,6 +73,93 @@ use std::cmp::{min, max};
 #[allow(unused_imports)]
 use std::io::Write;
 
+static mut MOD: u64 = 1;
+
+type Matrix = Vec<Vec<u64>>;
+
+// Matrix Exponentiation
+fn mat_mul(a: &Matrix, b: &Matrix) -> Matrix {
+    assert!(a[0].len() == b.len());
+
+    let h = a.len();
+    let w = b[0].len();
+
+    let mut ret = vec![vec![0; w]; h];
+    for i in 0..h {
+        for j in 0..w {
+            for k in 0..b.len() {
+                unsafe {
+                    ret[i][j] += (a[i][k] * b[k][j]) % MOD;
+                    ret[i][j] %= MOD;
+                }
+            }
+        }
+    }
+    ret
+}
+
+fn mat_pow(a: &Matrix, mut n: u64) -> Matrix {
+    let mut a = a.clone();
+    let mut b = vec![vec![0; a.len()]; a.len()];
+    for i in 0..a.len() {
+        b[i][i] = 1;
+    }
+    while n>0 {
+        if n&1 > 0 {
+            b = mat_mul(&b, &a);
+        }
+        a = mat_mul(&a, &a);
+        n /= 2;
+    }
+    b
+}
+
+pub fn mod_pow(b: u64, p: u64) -> u64 {
+    unsafe {
+        if p == 0 {
+            return 1;
+        }
+        let mut ret = mod_pow(b * b % MOD, p / 2) % MOD;
+        if p % 2 == 1 {
+            ret = ret * b % MOD;
+        }
+        ret
+    }
+}
+
+
+
+#[doc = " [l, r)"]
+pub fn binary_search_by<F>(mut l: u64, mut r: u64, f: &F) -> Option<u64>
+where
+    F: Fn(u64) -> bool,
+{
+    assert!(l <= r);
+    let r_bound = r;
+    while r != l {
+        let m = l + (r - l) / 2;
+        if f(m) {
+            r = m;
+        } else {
+            l = m + 1;
+        }
+    }
+    if r == r_bound {
+        None
+    } else {
+        Some(r)
+    }
+}
+
+fn number_of_digit(mut n: u64) -> usize {
+    let mut num = 0;
+    while n>0 {
+        num += 1;
+        n/= 10;
+    }
+    num
+}
+
 fn main() {
     input!{
       l: u64,
@@ -80,9 +167,53 @@ fn main() {
       b: u64,
       m: u64,
     }
-    if l >= m {
-        println!("{}", 0);
-    } else {
-        println!("{}", 1);
+    unsafe {
+        MOD = m;
     }
+    const D: usize = 18;
+
+    let sd = number_of_digit(a);
+    let mut first_idx = vec![None; D+1];
+    let mut acm = vec![l; D+1];
+    acm[0] = 0;
+
+    for d in sd..D+1 {
+        let f = |mid| {
+            let v = a + mid*b;
+            return number_of_digit(v) >= d
+        };
+        if let Some(idx) = binary_search_by(0, l, &f) {
+            acm[d-1] = idx;
+            if number_of_digit(a + b*idx) == d {
+                first_idx[d] = Some(idx);
+            }
+        }
+    }
+    let mut ans = 0;
+    for d in 1..D+1 {
+        if first_idx[d].is_none() {
+            continue;
+        }
+        let idx = first_idx[d].unwrap();
+        let first = (a + idx*b) % m;
+        let td = 10u64.pow(d as u32) % m;
+        let v = vec![
+            vec![td,0,0],
+            vec![1,1,0],
+            vec![0,b,1],
+        ];
+        let fv = vec![
+            vec![0, first, 1],
+        ];
+
+        let cd = acm[d] - acm[d-1];
+        let power = mat_pow(&v, cd);
+        let ret = mat_mul(&fv, &power);
+        let x = ret[0][0];
+        ans *= mod_pow(10, cd * d as u64);
+        ans %= m;
+        ans += x;
+        ans %= m;
+    }
+    println!("{}", ans);
 }
