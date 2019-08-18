f909b7bbac422d4dace08a687f53d45603312bd3
masaki.arizuka <masaki.arizuka@gmail.com>
2019-08-18 17:38:58 +0900

    ARC047-B

--- atcoder/ARC047/B/main.rs
+++ atcoder/ARC047/B/main.rs
@@ -78,4 +78,48 @@ fn main() {
     macro_rules! puts {
         ($($format:tt)*) => (write!(out,$($format)*).unwrap());
     }
+
+    input!{
+      n: usize,
+      xys: [(i64,i64); n],
+    }
+
+    let xys: Vec<_> = xys.iter().map(|&(x, y)| (x+y, x-y)).collect();
+    let mut xs: Vec<_> = xys.iter().map(|v| v.0).collect();
+    xs.sort();
+    xs.dedup();
+    let mut ys: Vec<_> = xys.iter().map(|v| v.1).collect();
+    ys.sort();
+    ys.dedup();
+
+    let x1 = xs[0];
+    let x2 = xs[xs.len()-1];
+    let y1 = ys[0];
+    let y2 = ys[ys.len()-1];
+    let square_len = max(x2-x1, y2-y1);
+    assert!(square_len%2 == 0);
+
+    let min_x = xs[0];
+    let max_x = xs[xs.len()-1];
+    let min_y = ys[0];
+    let max_y = ys[ys.len()-1];
+
+    let possibles = [
+        (min_x + square_len/2, min_y + square_len/2),
+        (max_x - square_len/2, min_y + square_len/2),
+        (max_x - square_len/2, max_y - square_len/2),
+        (min_x + square_len/2, max_y - square_len/2),
+    ];
+
+    let rev_rotate = |x, y| ((x+y)/2, (x-y)/2);
+    for &(px, py) in possibles.iter() {
+        let p = rev_rotate(px, py);
+        let mut dists: Vec<_> = xys.iter().map(|v| rev_rotate(v.0, v.1)).map(|v| (p.0-v.0).abs() + (p.1-v.1).abs()).collect();
+        dists.sort();
+        dists.dedup();
+        if dists.len() == 1 {
+            return puts!("{} {}\n", p.0, p.1);
+        }
+    }
+    unreachable!();
 }
