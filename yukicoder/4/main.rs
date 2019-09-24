3b393ccb7d24154f587261a514a5d8578875d59e
masaki.arizuka <masaki.arizuka@gmail.com>
2019-09-24 14:37:24 +0900

    Save

---
+++ yukicoder/4/main.rs
@@ -0,0 +1,106 @@
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+
+    ($next:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($a:expr),*) => {
+        #[cfg(debug_assertions)]
+        writeln!(&mut std::io::stderr(), concat!("[DEBUG] ", $(stringify!($a), "={:?} "),*), $($a),*);
+    }
+}
+
+#[allow(unused_imports)]
+use std::cmp::{min, max};
+#[allow(unused_imports)]
+use std::io::{stdout, stdin, BufWriter, Write};
+
+fn main() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($($format:tt)*) => (write!(out,$($format)*).unwrap());
+    }
+
+    input!{
+      n: usize,
+      ws: [usize; n],
+    }
+    let ttl: usize = ws.iter().sum();
+    if ttl%2 == 1 {
+        return puts!("{}\n", "impossible");
+    }
+    let mut dp = vec![vec![false; ttl+1]; n+1];
+    dp[0][0] = true;
+    for i in 0..n {
+        for w in 0..ttl {
+            if w+ws[i] < ttl {
+                dp[i+1][w + ws[i]] = dp[i][w] || dp[i+1][w + ws[i]];
+            }
+            dp[i+1][w] = dp[i][w] || dp[i+1][w];
+        }
+    }
+
+    if dp[n][ttl/2] {
+        puts!("{}\n", "possible");
+    } else {
+        puts!("{}\n", "impossible");
+    }
+}
