95d229ee62957a61c8ca4bbfa5015c4fc7f9814d
masaki.arizuka <masaki.arizuka@gmail.com>
2020-05-05 17:43:31 +0900

    ABC165

---
+++ atcoder/ABC165/C/main.rs
@@ -0,0 +1,117 @@
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+
+    ($next:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($a:expr),*) => {
+        #[cfg(debug_assertions)]
+        writeln!(&mut std::io::stderr(), concat!("[DEBUG] ", $(stringify!($a), "={:?} "),*), $($a),*);
+    }
+}
+
+#[allow(unused_imports)]
+use std::cmp::{min, max};
+#[allow(unused_imports)]
+use std::io::{stdout, stdin, BufWriter, Write};
+
+type Record = (usize,usize,usize,usize);
+
+fn score(aa: &mut Vec<usize>, abcds: &Vec<Record>) -> usize {
+    let mut score = 0;
+    for &(a,b,c,d) in abcds.iter() {
+        if aa[b-1] - aa[a-1] == c {
+            score += d;
+        }
+    }
+    score
+}
+
+
+fn rec(i: usize, cur: usize, n: usize, m: usize, abcds: &Vec<Record>, aa: &mut Vec<usize>) -> usize {
+    if i == n {
+        return score(aa, abcds);
+    }
+    let mut score = 0;
+    aa[i] = cur;
+    for j in cur..=m {
+        score = max(rec(i+1, j, n, m, abcds, aa), score);
+    }
+    score
+}
+
+fn main() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($($format:tt)*) => (write!(out,$($format)*).unwrap());
+    }
+
+    input!{
+      n: usize,
+      m: usize,
+      q: usize,
+      abcds: [(usize,usize,usize,usize); q],
+    }
+    let mut aa = vec![0; n];
+    let cur = 1;
+    let ans = rec(0, cur, n, m, &abcds, &mut aa);
+    puts!("{}\n", ans);
+}
---
+++ atcoder/ABC165/F/main.rs
@@ -0,0 +1,139 @@
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+
+    ($next:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! debug {
+    ($($a:expr),*) => {
+        #[cfg(debug_assertions)]
+        writeln!(&mut std::io::stderr(), concat!("[DEBUG] ", $(stringify!($a), "={:?} "),*), $($a),*);
+    }
+}
+
+#[allow(unused_imports)]
+use std::cmp::{min, max};
+#[allow(unused_imports)]
+use std::io::{stdout, stdin, BufWriter, Write};
+
+pub fn lower_bound<T: Ord>(a: &Vec<T>, x: &T) -> usize {
+    use std::cmp::Ordering;
+    let mut l = 0;
+    let mut r = a.len();
+    while l != r {
+        let m = l + (r - l) / 2;
+        match a[m].cmp(x) {
+            Ordering::Less => l = m + 1,
+            Ordering::Equal | Ordering::Greater => r = m,
+        }
+    }
+    l
+}
+
+const INF: usize = 1 << 32;
+
+fn dfs(cur: usize, parent: usize, edges: &Vec<Vec<usize>>, aa: &Vec<usize>, ans: &mut Vec<usize>, dp: &mut Vec<usize>) {
+    ans[cur] = lower_bound(&dp, &INF); // Get the current lis
+
+    for &child in edges[cur].iter() {
+        if child != parent {
+            let a = aa[child];
+            let idx = lower_bound(dp, &a);
+            let prev = dp[idx];
+            if dp[idx] >= a {
+                dp[idx] = a;
+            }
+            dfs(child, cur, edges, aa, ans, dp);
+            dp[idx] = prev;
+        }
+    }
+}
+
+fn main_() {
+    let out = std::io::stdout();
+    let mut out = BufWriter::new(out.lock());
+    macro_rules! puts {
+        ($($format:tt)*) => (write!(out,$($format)*).unwrap());
+    }
+    input!{
+      n: usize,
+      aa: [usize; n],
+      vs: [(usize1,usize1); n-1]
+    }
+    let mut edges = vec![vec![]; n];
+    for &(a,b) in vs.iter() {
+        edges[a].push(b);
+        edges[b].push(a);
+    }
+
+    let mut dp = vec![INF; n];
+    let mut ans = vec![0; n];
+    dp[0] = aa[0];
+    dfs(0, n, &edges, &aa, &mut ans, &mut dp);
+    for a in ans {
+        puts!("{}\n", a);
+    }
+}
+
+fn main() {
+    // In order to avoid potential stack overflow, spawn a new thread.
+    let stack_size = 104_857_600; // 100 MB
+    let thd = std::thread::Builder::new().stack_size(stack_size);
+    thd.spawn(|| main_()).unwrap().join().unwrap();
+}
